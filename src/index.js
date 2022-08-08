9fa6fcf134e9353f8c046302cbcac5bc4e6d1070
marivero-laya <laya.mvla@gmail.com>
2022-08-08 14:39:19 +0100

    Display the forecast data using API
    Forecast API integration

--- src/index.js
+++ src/index.js
@@ -61,27 +61,44 @@ function changeCelcius(event) {
 }
 
 // Forecast
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
           <div class="col-2" id="day-forecast">
-            <h3>${day}</h3>
+            <h3>${formatDay(forecastDay.dt)}</h3>
             <img
-                  src="https://openweathermap.org/img/wn/50d@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   width="42"
                 />
             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature.min">19°|</span>
-              <span class="weather-forecast-temperature.min">26°</span>  
+              <span class="weather-forecast-temperature.min">${Math.round(
+                forecastDay.temp.min
+              )}°|</span>
+              <span class="weather-forecast-temperature.min">${Math.round(
+                forecastDay.temp.max
+              )}°</span>  
             </div>
           </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
